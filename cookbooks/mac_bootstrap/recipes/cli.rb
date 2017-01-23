4ede544b708cadb70f5523a274116c1a8b4f2108
zdk <zdkaser@gmail.com>
2017-01-23 19:26:54 +0700

    Install more stuff

--- cookbooks/mac_bootstrap/recipes/cli.rb
+++ cookbooks/mac_bootstrap/recipes/cli.rb
@@ -27,6 +27,17 @@ template "/Users/di/.zshrc" do
   })
 end
 
-#Ruby
+# Ruby
 brew "rbenv"
 brew "ruby-build"
+
+# Vim Vundle
+bash "Install Vundle" do
+  code <<-EOH
+mkdir -p ~/.vim/bundle/ && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && vim +PluginInstall +qall
+  EOH
+  not_if { File.exists?("/Users/di/.vim/bundle") }
+end
+
+# Vagrant
+cask "vagrant"
