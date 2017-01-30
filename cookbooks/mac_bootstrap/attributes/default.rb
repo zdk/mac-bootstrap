1b9dd1656b3e883f528cc452337707645683b1f6
di <di@omise.co>
2017-01-30 12:32:41 +0700

    Configure cask packages as attribute

--- cookbooks/mac_bootstrap/attributes/default.rb
+++ cookbooks/mac_bootstrap/attributes/default.rb
@@ -1,1 +1,3 @@
 default["user"] = "di"
+default["brew_cask"]["packages"] = ["iterm2", "google-chrome", "slack", "1password", "docker-toolbox",
+                                    "skitch", "viscosity", "vagrant", "aws-vault"]
--- cookbooks/mac_bootstrap/recipes/cli.rb
+++ cookbooks/mac_bootstrap/recipes/cli.rb
@@ -2,31 +2,6 @@ package "git"
 package "tree"
 package "vim"
 
-# Oh-My-ZSH
-git "/Users/di/.oh-my-zsh" do
-  repository 'git://github.com/robbyrussell/oh-my-zsh.git'
-  user "di"
-  reference "master"
-  action :sync
-end
-
-execute "cp /Users/di/.zshrc /Users/di/.zshrc.orig" do
-  only_if { File.exists?("/Users/di/.zshrc") }
-end
-
-execute "cp /Users/di/.oh-my-zsh/templates/zshrc.zsh-template /Users/di/.zshrc"
-
-template "/Users/di/.zshrc" do
-  source "zshrc.erb"
-  owner "di"
-  mode "644"
-  action :create_if_missing
-  variables({
-    theme: "sunrise",
-    plugins: "git bundler osx rake ruby",
-  })
-end
-
 # Ruby
 brew "rbenv"
 brew "ruby-build"
@@ -39,9 +14,6 @@ mkdir -p ~/.vim/bundle/ && git clone https://github.com/VundleVim/Vundle.vim.git
   not_if { File.exists?("/Users/di/.vim/bundle") }
 end
 
-# Vagrant
-cask "vagrant"
 
 # AWS
 brew "awscli"
-cask "aws-vault"
